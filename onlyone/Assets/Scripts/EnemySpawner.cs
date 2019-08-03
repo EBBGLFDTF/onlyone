3ff7866bf54bc558b43f758faac83698f0cee097
PitEG <enriqueiigan@gmail.com>
2019-08-03 16:56:09 -0700

    v0.1
    Addition of a simple player controller with projectiles, enemy spawning, and interaction between enemies and projeciles

---
+++ onlyone/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	//not currently using 
+	enum EnemySpeeds {
+		slow, normal, fast 
+	}
+
+	public int spawnTimer = 180;
+	private int spawnCounter;
+
+	public GameObject enemy;
+	public int[] speeds = { 5, 10, 20 };
+
+	//position
+	public int lowerBound = -3;
+	public int upperBound = 4;
+	public int xPos = 5;
+
+	// Start is called before the first frame update
+	void Start() {
+		this.spawnCounter = this.spawnTimer;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		SpawnEnemy();
+	}
+
+	void SpawnEnemy() {
+		spawnCounter--;
+		if (spawnCounter <= 0) {
+			float yPos = Random.value * (upperBound - lowerBound) + lowerBound;
+			GameObject enemy = Instantiate(this.enemy, new Vector3(xPos, yPos, 0), Quaternion.identity);
+			float speed = speeds[Random.Range(0, speeds.Length)];
+			enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+			Destroy(enemy, 6);
+			this.spawnCounter = this.spawnTimer; 
+		}
+	}
+}
+
---
+++ onlyone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public KeyCode oneButton;
+	public GameObject projectile;
+	public int projectileSpeed;
+
+	private Transform transform;
+
+	// Start is called before the first frame update
+	void Start() {
+		transform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Shoot();
+	}
+
+	private void Shoot() {
+		if (Input.GetKeyDown(oneButton)){
+			Debug.Log("Shooooot");
+			GameObject projectileInstance = Instantiate(projectile);
+			projectileInstance.GetComponent<Transform>().position = transform.position;
+			projectileInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 25);
+			Destroy(projectileInstance, 3);
+		}
+	}
+}
